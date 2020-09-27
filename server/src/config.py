a7e76eacab254b3e1f2f2360392f875ec233d962
parthparikh <parthparikh1999p@gmail.com>
2020-09-27 23:30:48 +0530

    Update BASE_MODEL_PATH

--- server/src/config.py
+++ server/src/config.py
@@ -5,7 +5,7 @@ class config:
     TRAIN_BATCH_SIZE = 32
     VALID_BATCH_SIZE = 8
     EPOCHS = 5
-    BASE_MODEL_PATH = "../model/"
+    BASE_MODEL_PATH = "bert-base-uncased"
     MODEL_PATH = "../results/model.bin"
     TRAINING_FILE = "../data/ner_dataset.csv"
     TOKENIZER = transformers.BertTokenizer.from_pretrained(
